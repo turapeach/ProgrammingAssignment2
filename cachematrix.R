ea090f688d68f1edf10b2e6fc12692fcebee3e4c
turapeach <mike.johnson@mu.edu>
2014-12-16 07:03:47 +0800

    Update cachematrix.R
    Deleted extra text at end (from original file text)

--- cachematrix.R
+++ cachematrix.R
@@ -43,8 +43,3 @@ cacheSolve <- function(x) {
       x$setinv(xinv)             # Sets CacheMatrix xinverse value
       xinv
 }
-
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
-}
